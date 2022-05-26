7ef753279c1e041ec3ecd200ffcce7ae9369f74a
devilb2103 <43474979+devilb2103@users.noreply.github.com>
2022-05-27 00:33:50 +0530

    commit 1 idek

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 6969;
+
+app.use(express.json())
+
+var chats = [1]
+
+function clearChat() {
+    if(chats.length != 0){
+        chats = [];
+    }
+}
+
+app.get("/", (req, res) => {
+    res.send("server is up and running")
+});
+
+app.get("/chat", (req, res) => {
+    res.send(chats)
+});
+
+app.post("/schat", (req, res) => {
+    const msg = {
+        id: chats.length + 1,
+        name: req.body.name
+    };
+    if(msg.name == ">cls") {
+        clearChat()
+    }
+    else{
+        chats.push(msg)
+    }
+    res.send(msg);
+});
+
+app.listen(port, () => console.log("listening at port...."));
